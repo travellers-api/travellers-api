9d83d132cb7e8684138bdb3ca56971085f9f8e1b
Amon Sawamura <amotarao@gmail.com>
2022-11-29 16:00:20 +0900

    Firestore より cookie 取得

--- packages/functions/src/handlers/crawlAddressHomes.ts
+++ packages/functions/src/handlers/crawlAddressHomes.ts
@@ -1,12 +1,17 @@
 import { FunctionBuilder } from 'firebase-functions';
 import { getCookie } from '../modules/address/authentication';
 import { getHome } from '../modules/address/home';
-import { getAddressSecret } from '../modules/firestore';
+import { getAddressSecret, setAddressSecret } from '../modules/firestore';
 
 export const crawlAddressHomesHandler: Parameters<FunctionBuilder['https']['onRequest']>[0] = async (req, res) => {
   try {
-    const { email, password } = await getAddressSecret('amon');
-    const cookie = await getCookie({ email, password });
+    const secret = await getAddressSecret('amon');
+    const cookie =
+      secret.cookie ||
+      (await getCookie(secret).then(async (cookie) => {
+        await setAddressSecret('amon', { cookie });
+        return cookie;
+      }));
 
     const home = await getHome(req.query.id as string, cookie);
     res.json({ home });
