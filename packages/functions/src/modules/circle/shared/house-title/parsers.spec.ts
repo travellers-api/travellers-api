07153bb03db41b6537b55cfa5e5de15267f7f1a2
Amon Sawamura <amotarao@gmail.com>
2023-03-24 08:25:55 +0900

    test: use describe

--- packages/functions/src/modules/circle/shared/house-title/parsers.spec.ts
+++ packages/functions/src/modules/circle/shared/house-title/parsers.spec.ts
@@ -1,15 +1,19 @@
 import { parseHomeTitle } from './parsers';
 
-test('parseHomeTitle', () => {
-  expect(parseHomeTitle('【北海道安平町 / 相部屋（男女共用）】VACILANDO')).toEqual({
-    name: 'VACILANDO',
-    city: '北海道安平町',
-    roomType: '相部屋（男女共用）',
+describe('parseHomeTitle', () => {
+  test('通常', () => {
+    expect(parseHomeTitle('【北海道安平町 / 相部屋（男女共用）】VACILANDO')).toEqual({
+      name: 'VACILANDO',
+      city: '北海道安平町',
+      roomType: '相部屋（男女共用）',
+    });
   });
 
-  expect(parseHomeTitle('【高知県東洋町/ 個室】生見邸')).toEqual({
-    name: '生見邸',
-    city: '高知県東洋町',
-    roomType: '個室',
+  test('スペースがない', () => {
+    expect(parseHomeTitle('【高知県東洋町/ 個室】生見邸')).toEqual({
+      name: '生見邸',
+      city: '高知県東洋町',
+      roomType: '個室',
+    });
   });
 });
