cd2152a5d639bc22252e7f3d7c62819598c39f9d
Amon Sawamura <amotarao@gmail.com>
2023-03-24 08:58:41 +0900

    滞在予定: ADDress, circle 対応

--- packages/app/src/pages/a/[screenName].tsx
+++ packages/app/src/pages/a/[screenName].tsx
@@ -1,27 +1,7 @@
-import dayjs from 'dayjs';
 import { GetStaticPaths, GetStaticProps, NextPage } from 'next';
-import Head from 'next/head';
-import { useMemo } from 'react';
-
-type Reservation = {
-  id: string;
-  checkInDate: string;
-  checkOutDate: string;
-  status: 'pending' | 'approved' | 'staying' | 'stayed' | 'canceled' | null;
-  home: {
-    id: string;
-    name: string;
-    room: string;
-  };
-};
 
 export type Params = {
   screenName: string;
-};
-
-export type Props = {
-  screenName: string;
-  reservations: Reservation[];
 };
 
 export const getStaticPaths: GetStaticPaths<Params> = () => {
@@ -31,86 +11,18 @@ export const getStaticPaths: GetStaticPaths<Params> = () => {
   };
 };
 
-export const getStaticProps: GetStaticProps<Props, Params> = async ({ params }) => {
-  const screenName = params?.screenName ?? '';
-  if (!screenName) {
-    return {
-      notFound: true,
-      revalidate: 0,
-    };
-  }
-
-  const res = await fetch(`https://api.traveller-api.amon.dev/address/users/${screenName}/reservations`);
-  if (!res.ok) {
-    return {
-      notFound: true,
-      revalidate: 0,
-    };
-  }
-
-  const { reservations } = (await res.json()) as {
-    reservations: Reservation[];
-  };
-
+export const getStaticProps: GetStaticProps<Record<string, never>, Params> = async ({ params }) => {
   return {
-    props: {
-      screenName,
-      reservations: reservations.filter(
-        (reservation) => reservation.status === 'approved' || reservation.status === 'staying'
-      ),
+    redirect: {
+      statusCode: 308,
+      destination: `/u/${params?.screenName}`,
     },
     revalidate: 60 * 60,
   };
 };
 
-const Page: NextPage<Props> = ({ screenName, reservations }) => {
-  const title = useMemo(() => `${screenName}のADDress滞在予定`, [screenName]);
-
-  return (
-    <div className="pb-80">
-      <Head>
-        <title>{title}</title>
-      </Head>
-      <header className="mx-auto mb-20 flex max-w-480 flex-col gap-10 px-20 py-20">
-        <h1 className="text-center font-sans text-xl font-bold tracking-wide opacity-80">
-          {screenName}のADDress滞在予定
-        </h1>
-        <div className="text-center font-sans text-sm tracking-wide opacity-80">
-          <p>「避けたい」にご活用ください</p>
-          <p>
-            <a className="underline" href="https://twitter.com/amotarao">
-              Twitter @amotarao
-            </a>
-          </p>
-        </div>
-      </header>
-      <section className="mx-auto max-w-480 px-20">
-        <ul className="flex flex-col gap-20">
-          {reservations.map((reservation) => (
-            <li key={reservation.id}>
-              <div className="grid min-h-[120px] grid-cols-[100px_1fr] rounded-3xl border-2 p-20">
-                <div className="font-sans text-xs font-bold opacity-50">
-                  <p>
-                    <time dateTime={reservation.checkInDate}>
-                      {dayjs(reservation.checkInDate).format('MM.DD ddd')}-
-                    </time>
-                  </p>
-                  <p>
-                    <time dateTime={reservation.checkOutDate}>
-                      {dayjs(reservation.checkOutDate).format('MM.DD ddd')}
-                    </time>
-                  </p>
-                </div>
-                <p className="border-l-2 pl-20">
-                  <span className="font-sans text-3xl font-bold opacity-80">{reservation.home.name}</span>
-                </p>
-              </div>
-            </li>
-          ))}
-        </ul>
-      </section>
-    </div>
-  );
+const Page: NextPage = () => {
+  return null;
 };
 
 export default Page;
---
+++ packages/app/src/pages/u/[screenName].tsx
@@ -0,0 +1,141 @@
+import dayjs from 'dayjs';
+import { GetStaticPaths, GetStaticProps, NextPage } from 'next';
+import Head from 'next/head';
+import { useMemo } from 'react';
+
+type AddressReservation = {
+  id: string;
+  checkInDate: string;
+  checkOutDate: string;
+  status: 'pending' | 'approved' | 'staying' | 'stayed' | 'canceled' | null;
+  home: {
+    id: string;
+    name: string;
+    room: string;
+  };
+};
+
+type CircleReservation = {
+  id: string;
+  status: 'pending' | 'approved' | 'staying' | 'stayed' | 'canceled' | null;
+  checkInDate: string;
+  checkOutDate: string;
+  home: {
+    name: string;
+    city: string;
+    roomType: string;
+  };
+};
+
+export type Params = {
+  screenName: string;
+};
+
+export type Props = {
+  screenName: string;
+  reservations: ({ service: 'ADDress'; data: AddressReservation } | { service: 'circle'; data: CircleReservation })[];
+};
+
+export const getStaticPaths: GetStaticPaths<Params> = () => {
+  return {
+    paths: [],
+    fallback: 'blocking',
+  };
+};
+
+export const getStaticProps: GetStaticProps<Props, Params> = async ({ params }) => {
+  const screenName = params?.screenName ?? '';
+  if (!screenName) {
+    return {
+      notFound: true,
+      revalidate: 0,
+    };
+  }
+
+  const [address, circle] = await Promise.all([
+    fetch(`https://api.traveller-api.amon.dev/address/users/${screenName}/reservations`).then(async (res) => {
+      const { reservations } = (await res.json()) as { reservations: AddressReservation[] };
+      return { ok: res.ok, reservations };
+    }),
+    fetch(`https://api.traveller-api.amon.dev/circle/users/${screenName}/reservations`).then(async (res) => {
+      const { reservations } = (await res.json()) as { reservations: CircleReservation[] };
+      return { ok: res.ok, reservations };
+    }),
+  ]);
+
+  if (!address.ok && !circle.ok) {
+    return {
+      notFound: true,
+      revalidate: 0,
+    };
+  }
+
+  const props: Props = {
+    screenName,
+    reservations: [
+      ...address.reservations.map((data) => ({ service: 'ADDress' as const, data })),
+      ...circle.reservations.map((data) => ({ service: 'circle' as const, data })),
+    ]
+      .sort((a, z) => (a.data.checkInDate > z.data.checkInDate ? 1 : -1))
+      .filter((reservation) => reservation.data.status === 'approved' || reservation.data.status === 'staying'),
+  };
+
+  return {
+    props,
+    revalidate: 60 * 60,
+  };
+};
+
+const Page: NextPage<Props> = ({ screenName, reservations }) => {
+  const title = useMemo(() => `${screenName}の滞在予定`, [screenName]);
+
+  return (
+    <div className="pb-80">
+      <Head>
+        <title>{title}</title>
+      </Head>
+      <header className="mx-auto mb-20 flex max-w-480 flex-col gap-10 px-20 py-20">
+        <h1 className="text-center font-sans text-xl font-bold tracking-wide opacity-80">{screenName}の滞在予定</h1>
+        <div className="text-center font-sans text-sm tracking-wide opacity-80 [&_a]:underline">
+          <p>「避けたい」にご活用ください</p>
+          <p>
+            <a href="https://twitter.com/amotarao">Twitter @amotarao</a>
+          </p>
+        </div>
+      </header>
+      <section className="mx-auto max-w-480 px-20">
+        <ul className="flex flex-col gap-20">
+          {reservations.map((reservation) => (
+            <li key={`${reservation.service}_${reservation.data.id}`}>
+              <div className="grid min-h-[120px] grid-cols-[100px_1fr] rounded-3xl border-2 p-20">
+                <div className="font-sans text-xs font-bold text-black/50">
+                  <p>
+                    <time dateTime={reservation.data.checkInDate}>
+                      {dayjs(reservation.data.checkInDate).format('MM.DD ddd')}-
+                    </time>
+                  </p>
+                  <p>
+                    <time dateTime={reservation.data.checkOutDate}>
+                      {dayjs(reservation.data.checkOutDate).format('MM.DD ddd')}
+                    </time>
+                  </p>
+                </div>
+                <div className="grid grid-cols-1 content-start gap-8 border-l-2 pl-20">
+                  <p className="font-sans text-3xl font-bold opacity-80 line-clamp-1">{reservation.data.home.name}</p>
+                  <div className="flex gap-8">
+                    <p className="font-sans text-xs font-bold text-black/50">{reservation.service}</p>
+                    {reservation.service === 'circle' && (
+                      <p className="font-sans text-xs font-bold text-black/50">{reservation.data.home.city}</p>
+                    )}
+                  </div>
+                </div>
+              </div>
+            </li>
+          ))}
+        </ul>
+      </section>
+    </div>
+  );
+};
+
+export default Page;
