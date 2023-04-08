f2cac74503daae170556fd0c7c7e596e51ba6cb9
Amon Sawamura <amotarao@gmail.com>
2023-04-08 14:32:04 +0900

    circle-fetcher: 日付比較して完了になった予約も取得

--- packages/circle-fetcher/src/core/reservation/index.ts
+++ packages/circle-fetcher/src/core/reservation/index.ts
@@ -1,7 +1,11 @@
 import { fetchReservations } from './fetchers';
 import { Reservation } from './types';
+import { filterByAfterNow, sortByCheckInDate } from './utils';
 
-export const getReservations = async (cookie: string): Promise<Reservation[]> => {
-  const reservations = fetchReservations(cookie);
-  return reservations;
+export const getReservations = async (cookie: string, now = new Date()): Promise<Reservation[]> => {
+  const [reservingReservations, doneReservations] = await Promise.all([
+    fetchReservations(cookie, 'reserving'),
+    fetchReservations(cookie, 'done'),
+  ]);
+  return [...reservingReservations, ...doneReservations].sort(sortByCheckInDate).filter(filterByAfterNow(now));
 };
