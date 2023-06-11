d870d4ca4ba6d6c13c032cbb0d46b42760d71b3e
Amon Sawamura <amotarao@gmail.com>
2023-06-11 22:33:29 +0900

    ADDress: カレンダーフィルタ処理をAPIに移動

--- packages/app/src/app/address-calendar/page.tsx
+++ packages/app/src/app/address-calendar/page.tsx
@@ -1,169 +1,48 @@
-import dayjs from 'dayjs';
 import { Metadata } from 'next';
 import Link from 'next/link';
 import { notFound } from 'next/navigation';
 import { CalendarFilter } from '../../components/address-calendar/CalendarFilter';
 import { CalendarSection } from '../../components/address-calendar/CalendarSection';
-import { fetchCalendar } from '../../lib/address/calendar/fetchers';
-import { Home } from '../../lib/address/calendar/types';
-import { excludeClosedRooms } from '../../lib/address/calendar/utils';
-import { prefectures } from '../../lib/prefecture/constants';
-import { queryToArray } from '../../utils/router';
+import { AddressCalendarForPage } from './api/calendar/route';
 
-type Props = {
-  homes: Home[];
-  dates: {
-    date: string;
-    day: 0 | 1 | 2 | 3 | 4 | 5 | 6;
-  }[];
-  filters: {
-    prefecture: string[];
-    homeType: string[];
-    roomType: string[];
-    sex: { name: string; value: string }[];
+type PageContext = {
+  searchParams?: {
+    prefecture?: string | string[];
+    homeType?: string | string[];
+    roomType?: string | string[];
+    sex?: string | string[];
   };
 };
 
-const getData = async (searchParams?: { [key: string]: string | string[] | null }): Promise<Props> => {
-  const homes = await fetchCalendar({
-    next: { revalidate: 60 },
-  }).catch(() => null);
-  if (!homes) {
-    notFound();
-  }
-
-  const now = dayjs();
-  const dates = [...Array(60)].map((_, i) => {
-    const dayjsObj = now.add(i, 'days');
-    return {
-      date: dayjsObj.format('YYYY/MM/DD'),
-      day: Number(dayjsObj.format('d')) as 0 | 1 | 2 | 3 | 4 | 5 | 6,
-    };
-  });
-
-  const prefectureQuery = queryToArray(
-    searchParams && 'prefecture' in searchParams ? (searchParams.prefecture as string | string[] | null) : null
-  );
-  const homeTypeQuery = queryToArray(
-    searchParams && 'homeType' in searchParams ? (searchParams.homeType as string | string[] | null) : null
-  );
-  const roomTypeQuery = queryToArray(
-    searchParams && 'roomType' in searchParams ? (searchParams.roomType as string | string[] | null) : null
-  );
-  const sexQuery = queryToArray(
-    searchParams && 'sex' in searchParams ? (searchParams.sex as string | string[] | null) : null
-  );
-
-  return {
-    filters: {
-      prefecture: prefectures.map((prefecture) => prefecture.name),
-      homeType: (() => {
-        const values = new Set<string>();
-        homes.forEach((home) => {
-          values.add(home.homeType);
-        });
-        return Array.from(values).map((value) => value);
-      })(),
-      roomType: (() => {
-        const values = new Set<string>();
-        homes.forEach((home) => {
-          home.rooms.forEach((room) => {
-            values.add(room.type);
-          });
-        });
-        return Array.from(values).map((value) => value);
-      })(),
-      sex: [
-        { name: '男性', value: 'male' },
-        { name: '女性', value: 'female' },
-      ],
-    },
-    homes: homes
-      .map((home) => {
-        home.rooms = excludeClosedRooms(home.rooms);
-
-        if (roomTypeQuery) {
-          home.rooms = home.rooms.filter((room) => roomTypeQuery.includes(room.type));
-        }
-
-        if (sexQuery) {
-          home.rooms = home.rooms.filter((room) => sexQuery.some((q) => [q, null].includes(room.sex)));
-        }
-
-        home.rooms = home.rooms.map((room) => {
-          const calendar = room.calendar;
-          const availables = calendar
-            ? dates
-                .map((date) => {
-                  const isUnavailable = calendar.reservedDates.includes(date.date);
-                  if (isUnavailable) {
-                    return 'N';
-                  }
-
-                  const isOutOfTerm =
-                    date.date < (calendar.calStartDate || '0000/01/01') ||
-                    date.date > (calendar.calEndDate || '9999/12/31');
-                  if (isOutOfTerm) {
-                    return 'O';
-                  }
-
-                  const isHoliday = calendar.holidays.includes(date.day);
-                  if (isHoliday) {
-                    return 'H';
-                  }
-
-                  return 'Y';
-                })
-                .join('')
-            : null;
-
-          return {
-            ...room,
-            availables,
-          };
-        });
-
-        // 不要フィールド削除
-        home.thumbnail = '';
-        home.rooms.forEach((room) => {
-          room.thumbnail = '';
-        });
-
-        return home;
-      })
-      .filter((home) => {
-        if (prefectureQuery) {
-          const match = prefectureQuery.includes(home.prefecture);
-          if (!match) return false;
-        }
-
-        if (homeTypeQuery) {
-          const match = homeTypeQuery.includes(home.homeType);
-          if (!match) return false;
-        }
-
-        if (home.rooms.length === 0) {
-          return false;
-        }
-
-        return true;
-      })
-      .sort((a, z) => {
-        const aPrefecture = prefectures.find((prefecture) => a.prefecture === prefecture.name)?.code ?? 0;
-        const zPrefecture = prefectures.find((prefecture) => z.prefecture === prefecture.name)?.code ?? 0;
-
-        if (aPrefecture !== zPrefecture) {
-          return aPrefecture - zPrefecture;
-        }
-
-        return a.name > z.name ? 1 : -1;
-      }),
-    dates,
-  };
+const searchParamToArray = (value?: string | string[]): string[] => {
+  if (!value) return [];
+  if (typeof value === 'string') return [value];
+  return value;
 };
 
-export default async function Page({ searchParams }: { searchParams?: { [key: string]: string | string[] | null } }) {
-  const { homes, dates, filters } = await getData(searchParams);
+const getData = async (context: PageContext): Promise<AddressCalendarForPage> => {
+  const baseUrl = process.env.NEXT_PUBLIC_VERCEL_URL
+    ? `https://${process.env.NEXT_PUBLIC_VERCEL_URL}`
+    : 'http://localhost:3000';
+  const url = new URL(`${baseUrl}/address-calendar/calendar/api`);
+
+  const prefecture = searchParamToArray(context.searchParams?.prefecture);
+  prefecture.forEach((v) => url.searchParams.append('prefecture', v));
+  const homeType = searchParamToArray(context.searchParams?.homeType);
+  homeType.forEach((v) => url.searchParams.append('homeType', v));
+  const roomType = searchParamToArray(context.searchParams?.roomType);
+  roomType.forEach((v) => url.searchParams.append('roomType', v));
+  const sex = searchParamToArray(context.searchParams?.sex);
+  sex.forEach((v) => url.searchParams.append('sex', v));
+
+  const res = await fetch(url, { next: { revalidate: 60 } }).catch(() => null);
+  if (!res) notFound();
+  const json = await res.json();
+  return json;
+};
+
+export default async function Page(context: PageContext) {
+  const { homes, dates, filters } = await getData(context);
 
   return (
     <div className="pb-80">
