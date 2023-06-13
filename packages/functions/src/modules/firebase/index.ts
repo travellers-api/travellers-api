ce32cf17da3c873c255f397e276e8bd041516bd7
Amon Sawamura <amotarao@gmail.com>
2023-06-14 00:35:32 +0900

    functions: eslint rule を .eslintrc に移動

--- packages/functions/src/modules/firebase/index.ts
+++ packages/functions/src/modules/firebase/index.ts
@@ -1,6 +1,4 @@
-/* eslint-disable-next-line import/no-unresolved */
 import { initializeApp } from 'firebase-admin/app';
-/* eslint-disable-next-line import/no-unresolved */
 import { getFirestore } from 'firebase-admin/firestore';
 
 const app = initializeApp();
