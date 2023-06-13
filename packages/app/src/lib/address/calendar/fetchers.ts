ae716a951ccf50e8eccc9a4c551cf76b680b04a3
Amon Sawamura <amotarao@gmail.com>
2023-06-14 01:34:03 +0900

    app: add types

--- packages/app/src/lib/address/calendar/fetchers.ts
+++ packages/app/src/lib/address/calendar/fetchers.ts
@@ -1,6 +1,6 @@
 import { Home } from './types';
 
-export const fetchCalendar = async (fetchInit?: RequestInit) => {
+export const fetchCalendar = async (fetchInit?: RequestInit): Promise<Home[]> => {
   const res = await fetch(`https://api.traveller-api.amon.dev/address/calendar`, fetchInit);
 
   if (!res.ok) {
