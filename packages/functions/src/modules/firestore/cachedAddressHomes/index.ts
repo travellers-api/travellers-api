1d6dfadc2320fec20017c5f7a125bb18efa03665
Amon Sawamura <amotarao@gmail.com>
2023-07-03 09:56:40 +0900

    functions: collection types 利用

--- packages/functions/src/modules/firestore/cachedAddressHomes/index.ts
+++ packages/functions/src/modules/firestore/cachedAddressHomes/index.ts
@@ -1,10 +1,10 @@
-import { DocumentSnapshot, QuerySnapshot } from 'firebase-admin/firestore';
+import { CollectionReference } from 'firebase-admin/firestore';
 import { firestore } from '../../firebase';
 import { CachedAddressHome } from './types';
 
 export const collectionId = 'cachedAddressHomes';
 
-const collection = firestore.collection(collectionId);
+const collection = firestore.collection(collectionId) as CollectionReference<CachedAddressHome>;
 
 export const setHome = async (id: number, data: CachedAddressHome): Promise<void> => {
   await collection.doc(id.toString()).set(data);
@@ -19,7 +19,7 @@ export const deleteHome = async (id: number): Promise<void> => {
 };
 
 export const getHomes = async (): Promise<{ id: number; data: CachedAddressHome }[]> => {
-  const querySnapshot = (await collection.get()) as QuerySnapshot<CachedAddressHome>;
+  const querySnapshot = await collection.get();
   return querySnapshot.docs.map((snapshot) => ({
     id: Number(snapshot.id),
     data: snapshot.data(),
@@ -27,8 +27,8 @@ export const getHomes = async (): Promise<{ id: number; data: CachedAddressHome 
 };
 
 export const getHome = async (id: number): Promise<CachedAddressHome | null> => {
-  const snapshot = (await collection.doc(id.toString()).get()) as DocumentSnapshot<CachedAddressHome>;
+  const snapshot = await collection.doc(id.toString()).get();
   const data = snapshot.data();
   if (!data) return null;
-  return data as CachedAddressHome;
+  return data;
 };
