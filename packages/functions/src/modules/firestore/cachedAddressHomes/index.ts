7fe1d681ef185b5e0326e2ef71ea6a886b25fef8
Amon Sawamura <amotarao@gmail.com>
2023-07-03 09:25:56 +0900

    address: CachedAddressHome 型作成

--- packages/functions/src/modules/firestore/cachedAddressHomes/index.ts
+++ packages/functions/src/modules/firestore/cachedAddressHomes/index.ts
@@ -1,16 +1,16 @@
-import { Home } from '@travellers-api/address-fetcher/lib/core/home/types';
 import { DocumentSnapshot, QuerySnapshot } from 'firebase-admin/firestore';
 import { firestore } from '../../firebase';
+import { CachedAddressHome } from './types';
 
 export const collectionId = 'cachedAddressHomes';
 
 const collection = firestore.collection(collectionId);
 
-export const setHome = async (id: number, data: Home): Promise<void> => {
+export const setHome = async (id: number, data: CachedAddressHome): Promise<void> => {
   await collection.doc(id.toString()).set(data);
 };
 
-export const setHomePartial = async (id: number, data: Partial<Home>): Promise<void> => {
+export const setHomePartial = async (id: number, data: Partial<CachedAddressHome>): Promise<void> => {
   await collection.doc(id.toString()).set(data, { merge: true });
 };
 
@@ -18,17 +18,17 @@ export const deleteHome = async (id: number): Promise<void> => {
   await collection.doc(id.toString()).delete();
 };
 
-export const getHomes = async (): Promise<{ id: number; data: Home }[]> => {
-  const querySnapshot = (await collection.get()) as QuerySnapshot<Home>;
+export const getHomes = async (): Promise<{ id: number; data: CachedAddressHome }[]> => {
+  const querySnapshot = (await collection.get()) as QuerySnapshot<CachedAddressHome>;
   return querySnapshot.docs.map((snapshot) => ({
     id: Number(snapshot.id),
     data: snapshot.data(),
   }));
 };
 
-export const getHome = async (id: number): Promise<Home | null> => {
-  const snapshot = (await collection.doc(id.toString()).get()) as DocumentSnapshot<Home>;
+export const getHome = async (id: number): Promise<CachedAddressHome | null> => {
+  const snapshot = (await collection.doc(id.toString()).get()) as DocumentSnapshot<CachedAddressHome>;
   const data = snapshot.data();
   if (!data) return null;
-  return data as Home;
+  return data as CachedAddressHome;
 };
