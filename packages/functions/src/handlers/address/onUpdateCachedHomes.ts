7496ba17376e2ef161cfc9d2e77c02f086a1e700
Amon Sawamura <amotarao@gmail.com>
2023-08-10 20:22:20 +0900

    ADDress: functions: 型が合わない場合の Webhook キャンセル

--- packages/functions/src/handlers/address/onUpdateCachedHomes.ts
+++ packages/functions/src/handlers/address/onUpdateCachedHomes.ts
@@ -1,10 +1,16 @@
 import { Home } from '@travellers-api/address-fetcher/lib/core/home/types';
 import * as functions from 'firebase-functions';
 import { pick } from 'lodash';
+import { z } from 'zod';
 import { collectionId } from '../../modules/firestore/cachedAddressHomes';
 import { getWriteType } from '../../modules/firestore/write';
 import { defaultRegion } from '../../modules/functions/constants';
 import { publishDispatchHook } from '../../modules/hook/dispatch-hook';
+
+const homeZod = z.object({
+  id: z.string(),
+  name: z.string(),
+});
 
 export const onUpdateCachedHomes = functions
   .region(defaultRegion)
@@ -12,20 +18,41 @@ export const onUpdateCachedHomes = functions
   .onWrite(async (change) => {
     const type = getWriteType(change);
 
-    if (type === 'create') {
-      const home = change.after.data() as Home;
-      await publishDispatchHook({
-        topic: 'address.home.create',
-        data: pick(home, ['id', 'name']),
-      });
-      return;
-    }
-    if (type === 'delete') {
-      const home = change.before.data() as Home;
-      await publishDispatchHook({
-        topic: 'address.home.delete',
-        data: pick(home, ['id', 'name']),
-      });
-      return;
+    switch (type) {
+      case 'create': {
+        const home = change.after.data() as Home;
+        const data = pick(home, ['id', 'name']);
+
+        try {
+          homeZod.parse(data);
+        } catch (e) {
+          console.error(e);
+          break;
+        }
+
+        await publishDispatchHook({
+          topic: 'address.home.create',
+          data,
+        });
+        break;
+      }
+
+      case 'delete': {
+        const home = change.before.data() as Home;
+        const data = pick(home, ['id', 'name']);
+
+        try {
+          homeZod.parse(data);
+        } catch (e) {
+          console.error(e);
+          break;
+        }
+
+        await publishDispatchHook({
+          topic: 'address.home.delete',
+          data,
+        });
+        break;
+      }
     }
   });
