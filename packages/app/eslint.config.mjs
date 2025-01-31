ac884c982f152cdca714d146958a6171e56933d7
Amon Sawamura <amotarao@gmail.com>
2025-01-31 14:09:28 +0900

    add eslint plugins

--- packages/app/eslint.config.mjs
+++ packages/app/eslint.config.mjs
@@ -3,6 +3,8 @@
 import { FlatCompat } from "@eslint/eslintrc";
 import eslint from "@eslint/js";
 import eslintConfigPrettier from "eslint-config-prettier";
+import eslintPluginReact from "eslint-plugin-react";
+import eslintPluginReactHooks from "eslint-plugin-react-hooks";
 import tsEslint from "typescript-eslint";
 
 const compat = new FlatCompat({
@@ -10,6 +12,9 @@ const compat = new FlatCompat({
 });
 
 export default tsEslint.config(
+  {
+    ignores: [".next"],
+  },
   eslint.configs.recommended,
   tsEslint.configs.recommended,
   tsEslint.configs.stylistic,
@@ -34,5 +39,13 @@ export default tsEslint.config(
   ...compat.config({
     extends: ["next"],
   }),
+  eslintPluginReact.configs.flat.recommended,
+  eslintPluginReact.configs.flat["jsx-runtime"],
+  {
+    plugins: {
+      "react-hooks": eslintPluginReactHooks,
+    },
+    rules: { ...eslintPluginReactHooks.configs.recommended.rules },
+  },
   eslintConfigPrettier,
 );
