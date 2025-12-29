57ca8bbd1110b98c68f39b813c6b11bfef5fb6ee
Amon Sawamura <amotarao@gmail.com>
2025-12-29 12:35:19 +0900

    add vitest import

--- packages/functions/src/modules/address/index.spec.ts
+++ packages/functions/src/modules/address/index.spec.ts
@@ -1,3 +1,4 @@
+import { describe, expect, test } from "vitest";
 import { generateNumbers } from ".";
 import { dayjs } from "../../lib/dayjs";
 
--- packages/functions/src/modules/date/index.spec.ts
+++ packages/functions/src/modules/date/index.spec.ts
@@ -1,3 +1,4 @@
+import { describe, expect, test } from "vitest";
 import { addDays, addMonths, getDiffDays } from ".";
 
 describe("date", () => {
